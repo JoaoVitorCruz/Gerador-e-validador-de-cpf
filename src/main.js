42f2ff3448da1b50c1a609af9beab7103b9989cc
= <joaomauricio167@gmail.com>
2021-08-23 14:01:48 -0300

    'finalizado'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
 import { ValidaCpf } from './modules/ValidaCpf'
 import './assets/css/style.css';
 
-let c1 = new ValidaCpf('53282288808')
-console.log(ValidaCpf.geraCpf())
-console.log(ValidaCpf.geraDigitos('385233728'))
+const cpfGerado = document.querySelector('.gerar input')
+const btnGeraCpf = document.querySelector('.gerar .btn')
+const validarCpf = document.querySelector('.validar input')
+const btnValidarCpf = document.querySelector('.validar .btn') 
+const resultadoValidacao = document.querySelector('.validar .resultado')
+
+cpfGerado.value = ValidaCpf.geraCpf()
+
+btnGeraCpf.addEventListener('click', () => {
+    cpfGerado.value = ValidaCpf.geraCpf()
+})
+
+btnValidarCpf.addEventListener('click', ()=>{
+    const cpf = new ValidaCpf(validarCpf.value)
+    const isValid = cpf.valida()
+    let resultado 
+    
+    console.log(cpf)
+
+    if (cpf.cpfLimpo.length === 9 && cpf.digitos.length == 2) {
+    
+        if(isValid){
+            resultado = '<h2 class="success">Valido!</h2>'
+        }else {
+            resultado = '<h2 class="fail">Invalido!</h2>'
+        }
+    } else {
+        resultado = "<h2 class='warning'>Confira o CPF inserido</h2>"   
+    }
+    
+    resultadoValidacao.innerHTML = resultado
+})
